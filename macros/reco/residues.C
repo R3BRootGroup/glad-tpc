e82bde290420f1c085e9f90df8944bc0dfd6968e
David Garcia Allo <david.garcia.allo@rai.usc.es>
2022-07-29 12:40:45 +0200

    Debugging and cleaning up residues.C

--- macros/reco/residues.C
+++ macros/reco/residues.C
@@ -3,9 +3,10 @@
 ////////////////////////////////////////////////////////////////////////////////
 using namespace std;
 
+
 Double_t GetDistance(Double_t x1, Double_t z1, Double_t x2, Double_t z2){
   Double_t distance;
-  distance = (Double_t) TMath::Sqrt((x1-x2)*(x1-x2)+(z1-z2)*(z1-z2));
+  distance = TMath::Sqrt((x1-x2)*(x1-x2)+(z1-z2)*(z1-z2));
   return distance;
 }
 
@@ -23,16 +24,20 @@ void residues(TString simFilename = "sim.root", TString recoFilename = "output_r
   //Timer for runtime check
   TStopwatch timer;
   timer.Start();
+
   // Input files: simulation and reconstruction -> Compare Points and Hits
   TString simFilePath;
   TString recoFilePath;
+
   //Setting up env and paths
   TString workDir = gSystem->Getenv("VMCWORKDIR"); //Setting work directory
   simFilePath = workDir + "/glad-tpc/macros/sim/Prototype/" + simFilename; //File paths
   recoFilePath = workDir + "/glad-tpc/macros/reco/" + recoFilename;
+
   //Opening Files
   TFile* simFile = new TFile(simFilePath);
   TFile* recoFile = new TFile(recoFilePath);
+
   //Checking that opened both files
   if (simFile->IsOpen() && recoFile->IsOpen()){cout<<"[INFO] Files correctly opened!"<<endl;}
   else {cout<<"[ERROR] Files not opened!"<<endl; exit(1);}
@@ -65,20 +70,24 @@ void residues(TString simFilename = "sim.root", TString recoFilename = "output_r
   branchGTPCHitData = recoTree->GetBranch("GTPCHitData"); //Get the branch of Hits
   branchGTPCHitData->SetAddress(&GTPCHitDataCA); //GTPCHitDataCA ref to the branch
 
-  //Check we have same events in both files
+  //Checking if we have same events in both files
   Long64_t sim_events = simTree->GetEntries();
   cout<<"[INFO] Number of events in simulation is "<<sim_events<<endl;
   Long64_t reco_events = recoTree->GetEntries();
   cout<<"[INFO] Number of events in reconstruction is "<<reco_events<<endl;
+  if (sim_events != reco_events){
+    cout<<"[ERROR] Different number of events in sim and reco"<<endl;
+    exit(2);
+  }
 
-  //Canvas and histo fro visualizing residues
+  //Canvas and histo for visualizing residues
   TCanvas* c = new TCanvas("c","XZ Residues",0,500,500,500);
   TH1D* reshist = new TH1D("reshist", "Residues Histogram", 501,0,10);
-
   //Integers variables to store the number of different data in each event
   Int_t eventPoints;
   Int_t eventHits;
   Int_t eventTracks;
+
   //Main loop through all events
   for (Int_t i=0; i<sim_events; i++){
     //Getting the points in event i
@@ -92,14 +101,6 @@ void residues(TString simFilename = "sim.root", TString recoFilename = "output_r
     recoTree->GetEvent(i);
     eventHits = GTPCHitDataCA->GetEntries();
 
-    //Printing some info
-    /*if (i%10 == 0){
-      cout<<"\n[INFO] Event ID: "<<i<<";\tNumber of Points: "<<eventPoints<<endl;
-      cout<<"[INFO] Event ID: "<<i<<";\tNumber of Tracks: "<<eventTracks<<endl;
-      cout<<"[INFO] Event ID: "<<i<<";\tNumber of Hits: "<<eventHits<<endl;
-
-    }*/
-
     //Create points and hits arrays and fill them
     if (eventPoints > 0){
       points = new R3BGTPCPoint*[eventPoints];
@@ -110,8 +111,55 @@ void residues(TString simFilename = "sim.root", TString recoFilename = "output_r
       for (Int_t j=0; j<eventHits;j++) {hits[j] = (R3BGTPCHitData*)GTPCHitDataCA->At(j);}
     }
 
-    //One particular event
-    if (i==999){
+    //Create variables
+    Double_t x;
+    Double_t y;
+    Double_t z;
+    Int_t trackID;
+    Int_t tempID = 0;
+    Int_t k = 0;
+    TGraph* fitgr = new TGraph(); //Graph to plot take the primary track points to be fitted
+    TF1* fit = new TF1("fit", "pol2", 5, 15);// x -> [5,15]cm
+
+    //Loop through points in event
+    for (Int_t j=0; j<eventPoints;j++){
+      x = points[j]->GetX(); y = points[j]->GetY(); z = points[j]->GetZ();
+      trackID = points[j]->GetTrackID();
+      if (trackID == tempID){
+        fitgr->SetPoint(k,x,z);
+        k ++;
+      }
+    }
+    cout<<"[INFO] Fitting... (Event "<<i<<")"<<endl;
+    fitgr->Fit("fit", "Q"); //We have the fitted function -> Delete second arg to vis fit results (Q->Minimum printing)
+
+    //Creating points following the fit to calculate distances
+    Int_t n_fitpoints = 200;
+    Double_t fit_x[n_fitpoints];
+    Double_t fit_z[n_fitpoints];
+    TRandom* r3 = new TRandom3();
+    Double_t p;
+    for (Int_t j=0; j<n_fitpoints; j++){
+      p = r3->Rndm(j);
+      fit_x[j] = p*10+5;
+      fit_z[j] = fit->Eval(fit_x[j]);
+      //cout<<fit_x[j]<<"  "<<fit_z[j]<<endl;
+    }
+
+
+    Double_t energy;
+    Double_t distance;
+    for (Int_t j=0; j<eventHits; j++){
+      x = hits[j]->GetX(); y = hits[j]->GetY(); z = hits[j]->GetZ();
+      energy = hits[j]->GetEnergy();
+      distance = GetMinDistance(x,z,fit_x,fit_z);
+      reshist->Fill(distance, energy);
+
+    }
+
+    //Plot and info for one event -> Delete all between the ## when finished
+    //######################################################################################
+    /*if (i==(sim_events-1)){
       //Creating the canvas
       TCanvas* c1 = new TCanvas("c1","Event 2D",0,0,500,500);
       TCanvas* c2 = new TCanvas("c2","Event 3D",500,0,500,500);
@@ -152,15 +200,6 @@ void residues(TString simFilename = "sim.root", TString recoFilename = "output_r
           fitgr->SetPoint(k,x,z);
           k ++;
         }
-        /*else{//Draw all the secondary tracks
-          trackgr->Draw("same LINE");
-          trackgr = new TGraph2D();
-          trackgr->SetLineColor(trackID); //Different color for non primary tracks
-          trackgr->Set(1);
-          trackgr->SetPoint(0,x,y,z);
-          tempID = trackID;
-          k = 1;
-        }*/
       }
       cout<<"[INFO] Fitting... Event "<<i<<endl;
       c1->cd();
@@ -223,58 +262,8 @@ void residues(TString simFilename = "sim.root", TString recoFilename = "output_r
 
       c3->cd();
       res->Draw("HIST");
-    }
-
-
-    //Create variables
-    Double_t x;
-    Double_t y;
-    Double_t z;
-
-    Int_t trackID;
-    Int_t tempID = 0;
-    Int_t k = 0;
-
-    TGraph* fitgr = new TGraph(); //Graph to plot take the primary track points to be fitted
-
-    TF1* fit = new TF1("fit", "pol2", 5, 15);// x -> [5,15]cm
-
-
-    //Loop through points in event
-    for (Int_t j=0; j<eventPoints;j++){
-      x = points[j]->GetX(); y = points[j]->GetY(); z = points[j]->GetZ();
-      trackID = points[j]->GetTrackID();
-      if (trackID == tempID){
-        fitgr->SetPoint(k,x,z);
-        k ++;
-      }
-    }
-    cout<<"\n[INFO] Fitting... Event "<<i;
-    fitgr->Fit("fit"); //We have the fitted function
-
-    //Creating points following the fit to calculate distances
-    Int_t n_fitpoints = 200;
-    Double_t fit_x[n_fitpoints];
-    Double_t fit_z[n_fitpoints];
-    TRandom* r3 = new TRandom3();
-    Double_t p;
-    for (Int_t j=0; j<n_fitpoints; j++){
-      p = r3->Rndm(j);
-      fit_x[j] = p*10+5;
-      fit_z[j] = fit->Eval(fit_x[j]);
-      //cout<<fit_x[j]<<"  "<<fit_z[j]<<endl;
-    }
-
-
-    Double_t energy;
-    Double_t distance;
-    for (Int_t j=0; j<eventHits; j++){
-      x = hits[j]->GetX(); y = hits[j]->GetY(); z = hits[j]->GetZ();
-      energy = hits[j]->GetEnergy();
-      distance = GetMinDistance(x,z,fit_x,fit_z);
-      reshist->Fill(distance, energy);
-
-    }
+    }*/
+    //####################################################################################
 
     //Event i finishes ==> Deleting the arrays
     if (eventPoints)
